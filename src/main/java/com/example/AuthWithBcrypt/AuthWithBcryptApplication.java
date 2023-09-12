cbedbcf768e387a61c64786f58233d0e5a87cd54
mosubborini2000 <mosubborini2000@gmail.com>
2023-09-12 20:02:56 +0000

    new

---
+++ src/main/java/com/example/AuthWithBcrypt/AuthWithBcryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthWithBcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWithBcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWithBcryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthWithBcrypt/AuthWithBcryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthWithBcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWithBcryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
